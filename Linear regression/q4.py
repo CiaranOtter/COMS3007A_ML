bd8515bdf59625f41a2c4fe6f3ecbb2462786510
Ciaran <ciaranjotter@gmail.com>
2022-04-20 18:49:43 +0200

    regularisation working

--- Linear regression/q4.py
+++ Linear regression/q4.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+from scipy.special import logsumexp
 
 # ----- part a -----
 # ------ (i) ------
@@ -37,10 +38,11 @@ for i, d in enumerate(design_matrix):
 
 # ----- (v) -----
 
-def PlotScatter(theta, theta_grad, x):
+def PlotScatter(theta, theta_grad, theta_reg, x):
     plt.scatter(x_values, y_values)
     plt.plot(x[:,1], f(x[:,1],theta), "r")
     plt.plot(x[:,1], f(x[:,1], theta_grad), "g")
+    plt.plot(x[:,1], f(x[:,1], theta_reg), "y")
     plt.show()
 
 
@@ -82,11 +84,11 @@ def Error(theta):
     # err = err + np.dot(theta.transpose(), np.dot(design_matrix.transpose(),np.dot(design_matrix, theta)))
     # return err/2
     err = 0;
-    for i in range(len(y_values)):
-        temp = ( f(x_values[i],theta) - y_values[i])
-        err += temp*temp
+    for d in training:
+        temp = logsumexp((f(d[0],theta) - d[1] ))
+        err += ((temp**2)/2)
 
-    return err/2
+    return err
         
 print(Error(trained))
 
@@ -97,33 +99,102 @@ x_train = design_matrix[design_matrix[:,1].argsort()]
 
 # ----- (iv) -----
 
-def Gradient_Decent():
+def Plot_Time_Error(time_step, training_error):
+    plt.plot(time_step[:100:], training_error[:100:], "y")
+    plt.show()
+
+def Gradient_Decent(n):
     alpha = 0.0000001
-    e = 10**-6
+    e = 10**(-5)
 
-    theta_new = np.zeros(3)
-    theta_old = np.random.uniform(0,1,3)
+    theta_new = np.zeros(n)
+    theta_old = np.random.uniform(0,1,n)
 
-    while (np.linalg.norm(abs(theta_old-theta_new),2) > e):
-        theta_new = theta_old
-        theta_new[0] = theta_new[0] - Partial_Derivative(alpha,0, theta_old)
-        theta_new[1] = theta_new[1] - Partial_Derivative(alpha, 1, theta_old)
-        theta_new[2] = theta_new[2] - Partial_Derivative(alpha, 2, theta_old)
+    print(theta_new)
+    print(theta_old)
+
+    time_step = np.array([]).astype(int)
+    training_error = np.array([])
+    index = 0
+
+    while (np.linalg.norm(abs(theta_old-theta_new),2) >= e):
+        for i in range(n):
+            theta_old[i] = theta_new[i]
+            theta_new[i] = theta_new[i] - Partial_Derivative(alpha,i, theta_old)
     
+        time_step = np.append(time_step,index)
+        index = index +1
+        if (index%20 == 0):
+            training_error = np.append(training_error,Error(theta_new))
+    
+        
+
+    # print("time_step", time_step)
+    # print(index)
+    # print("training_error", training_error)
+    # Plot_Time_Error(time_step,training_error)
     return theta_new
     
 
 def Partial_Derivative(alpha, i, theta):
     err = 0;
-    for j,y in enumerate(y_values):
-        y_hat = f(x_values[j], theta)
-        err += alpha*(y_hat - y)*(x_values[j]**i)
-        
-    return err
+    for j,d in enumerate(training):
+        y_hat = f(d[0], theta)
+        err += (y_hat - d[1])*(d[0]**i)
+
+    return alpha*err
 
 print(theta)
 print(trained)
-gradient = Gradient_Decent()
+gradient = Gradient_Decent(3)
 print(gradient)
 
-PlotScatter(trained, gradient, x_train)
+
+
+
+# ----- part c -----
+# ----- {i} -----
+
+def Regularisation():
+
+    design_matrix = np.ones((len(x_values), 4))
+    for i,x in enumerate(x_values):
+        design_matrix[i][1] = x
+        design_matrix[i][2] = x*x
+        design_matrix[i][3] = x*x*x
+    
+    def Partial_Derivative_regularised(alpha, i, theta, l):
+        err = 0;
+        for j,d in enumerate(training):
+            y_hat = f(d[0], theta)
+            err += alpha*(y_hat - d[1])*(d[0]**i)
+
+        return err + alpha*l*theta[i]
+
+    def Gradient_Decent_Regularised():
+        alpha = 0.00000000001
+        e = 10**(-5)
+        l = 0.3
+
+        theta_old = np.zeros(4)
+        theta_new = np.random.uniform(0,1,4)
+        while (np.linalg.norm(abs(theta_old-theta_new),2) >= e):
+            for i in range(4):
+                theta_old[i] = theta_new[i]
+                pDer = Partial_Derivative_regularised(alpha, i, theta_old, l)
+                print(pDer)
+                theta_new[i] = theta_new[i] - pDer
+    
+            # time_step = np.append(time_step,index)
+            # index = index +1
+            # if (index%20 == 0):
+            #     training_error = np.append(training_error,Error(theta_new))
+    
+        return theta_new;
+
+    
+    cubic_theta = Gradient_Decent_Regularised()
+    PlotScatter(trained, gradient, cubic_theta, x_train)
+
+
+Regularisation()
