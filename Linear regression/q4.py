e20c567ec78698b8d5794cbda191d5723ed8c22c
Ciaran <ciaranjotter@gmail.com>
2022-04-20 22:23:27 +0200

    Completed Tut

--- Linear regression/q4.py
+++ Linear regression/q4.py
@@ -2,28 +2,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 from scipy.special import logsumexp
-
+# ----- QUESTION 4 -----
 # ----- part a -----
 # ------ (i) ------
+
+# Sample 150 x-values for the model
 
 x_values = np.random.normal(loc=0, scale=10, size=150)
 
 # ----- (ii) -----
+
+# compute the design matrix for the model
 
 design_matrix = np.ones((len(x_values), 3))
 for i,x in enumerate(x_values):
     design_matrix[i][1] = x
     design_matrix[i][2] = x*x
 
-# print(design_matrix)
-
 # ----- (iii) -----
+
+# sample three true value for Theta 
 
 theta = np.zeros(3)
 for i in range(3):
     theta[i] = np.random.uniform()  
 
 # ----- (iv) -----
+
+# use the design matrix and theta to calculate true y_values
 
 y_values = np.zeros(150)
 
@@ -34,19 +40,22 @@ def f(x, theta):
     return out
 
 for i, d in enumerate(design_matrix):
-    y_values[i] = f(x_values[i],theta)+np.random.normal(loc=0, scale=8, size=1)
+    y_values[i] = f(x_values[i],theta)+np.random.normal(loc=0, scale=8, size=1) # adding some noise to each data point
 
 # ----- (v) -----
 
-def PlotScatter(theta, theta_grad, theta_reg, x):
-    plt.scatter(x_values, y_values)
-    plt.plot(x[:,1], f(x[:,1],theta), "r")
-    plt.plot(x[:,1], f(x[:,1], theta_grad), "g")
-    plt.plot(x[:,1], f(x[:,1], theta_reg), "y")
-    plt.show()
+# plot a scatter plot of teh x and y values
 
+def PlotScatter():
+    plt.scatter(x_values, y_values)
+    plt.title("Scatter plot of generated x and y values")
+
+PlotScatter()
+plt.show()
 
 # ----- (vi) -----
+
+# split the data into traing testing and validation data
 
 def setdiff2d_list(arr1, arr2):
     out = []
@@ -68,6 +77,8 @@ testing = setdiff2d_list(temp, validation)
 # ----- part b -----
 # ----- (i) -----
 
+# use the Moore-penrose pseudo-inverse to calculate the values of theat
+
 Xt = design_matrix.transpose()
 
 trained = np.dot(Xt, design_matrix)
@@ -75,43 +86,55 @@ trained = np.linalg.inv(trained)
 trained = np.dot(trained, Xt)
 trained = np.dot(trained, y_values)
 
+print("Moore-penrose (closed form solution):")
+print("------------------------------------\n")
 
 # ----- (ii) -----
 
-def Error(theta):
-    # err = np.dot(y_values.transpose(), y_values)
-    # err = err - np.dot(theta.transpose(),np.dot(design_matrix.transpose(), y_values))
-    # err = err + np.dot(theta.transpose(), np.dot(design_matrix.transpose(),np.dot(design_matrix, theta)))
-    # return err/2
-    err = 0;
-    for d in training:
-        temp = logsumexp((f(d[0],theta) - d[1] ))
-        err += ((temp**2)/2)
+# calculate difference between true values and calculated values
+print("True value of Theta: ", theta)
+print("Moore-Penrose solution: ", trained)
+print("difference between True and predicted values: ", theta - trained)
+print("\n")
 
-    return err
-        
-print(Error(trained))
 
 # ----- (iii) -----
 
-x_train = design_matrix[design_matrix[:,1].argsort()]
+# calculate training and validation error for learned model
 
+def Error(theta, data):
+    err = 0;
+    for d in data:
+        temp = ( d[1] - f(d[0],theta))
+        err += (temp**2)
+
+    return err/2
+        
+print("Training error for Moore-Penrose: ", Error(trained, training))
+print("Validation error for Moore-Penrose: ", Error(trained, validation))
+print("\n")
 
 # ----- (iv) -----
 
-def Plot_Time_Error(time_step, training_error):
-    plt.plot(time_step[:100:], training_error[:100:], "y")
-    plt.show()
+# plot scatter polot for x and y values and the Moore-Penrose prediction in red
 
-def Gradient_Decent(n):
-    alpha = 0.0000001
-    e = 10**(-5)
+PlotScatter()
+x_train = design_matrix[design_matrix[:,1].argsort()]
+plt.plot(x_train[:,1], f(x_train[:,1], trained), "r")
+plt.title("Plot of scatter points and Moore-Penrose Prediction for theta (red)")
+plt.show()
+
+# ----- (v) -----
+# repeat previous stesp but for Gradient descent 
+
+# function for calculating gradient descent
+
+print("Gradient descent solution:")
+print("-------------------------\n")
+def Gradient_Decent(n, alpha, e, l):
 
     theta_new = np.zeros(n)
     theta_old = np.random.uniform(0,1,n)
-
-    print(theta_new)
-    print(theta_old)
 
     time_step = np.array([]).astype(int)
     training_error = np.array([])
@@ -120,81 +143,176 @@ def Gradient_Decent(n):
     while (np.linalg.norm(abs(theta_old-theta_new),2) >= e):
         for i in range(n):
             theta_old[i] = theta_new[i]
-            theta_new[i] = theta_new[i] - Partial_Derivative(alpha,i, theta_old)
+            theta_new[i] = theta_new[i] - Partial_Derivative(alpha,i, theta_old, 0)
     
         time_step = np.append(time_step,index)
         index = index +1
         if (index%20 == 0):
-            training_error = np.append(training_error,Error(theta_new))
+            training_error = np.append(training_error,Error(theta_new, training))
     
-        
+    # plot the training error over time
 
-    # print("time_step", time_step)
-    # print(index)
-    # print("training_error", training_error)
-    # Plot_Time_Error(time_step,training_error)
-    return theta_new
     
 
-def Partial_Derivative(alpha, i, theta):
+    return theta_new, time_step, training_error    
+
+def Partial_Derivative(alpha, i, theta, l):
     err = 0;
     for j,d in enumerate(training):
         y_hat = f(d[0], theta)
-        err += (y_hat - d[1])*(d[0]**i)
+        err += alpha*(y_hat - d[1])*(d[0]**i)+alpha*l*theta[i]
 
-    return alpha*err
+    return err
 
-print(theta)
-print(trained)
-gradient = Gradient_Decent(3)
-print(gradient)
+gradient, ts, te = Gradient_Decent(3, 0.0000001, 10**(-5), 0)
 
+# ----- [4b] (ii) {Gradient descent} ----- 
+# calculating difference between true values of theat and theta predicted by gradient descent
 
+print("True values of Theat: ", theta)
+print("Gradient descent Theta: ", gradient)
+print("difference between True and predicted values: ", theta- gradient)
+print("\n")
+# ----- [4b] (iii) {Gradient} ----- 
+# computing errrors in validation and training for gradient descent
 
+print("Training error for gradient descent: ", Error(gradient, training))
+print("Validation error for gradient descent: ", Error(gradient, validation))
+print("\n")
+
+# ----- [4b] (iv) {Gradient descent} -----
+# plotting the model
+
+PlotScatter()
+plt.plot(x_train[:,1], f(x_train[:,1], gradient), "r")
+plt.title("Plot of scatter points and Gradient descent Prediction for theta (red)")
+plt.show()
+
+# ----- [4b] (v) {Gradient descent} -----
+# plot error time 
+
+plt.plot(ts[:100:], te[:100:], "y")
+plt.title("plot for training error for regression model over time ")
+plt.show()
 
 # ----- part c -----
-# ----- {i} -----
+# ----- (i) -----
+# appending a third feature to design matrix 
 
-def Regularisation():
+print("Third Order gradient descent (no regularization): ")
+print("------------------------------------------------\n")
 
-    design_matrix = np.ones((len(x_values), 4))
-    for i,x in enumerate(x_values):
-        design_matrix[i][1] = x
-        design_matrix[i][2] = x*x
-        design_matrix[i][3] = x*x*x
+third_feature = np.zeros(150)
+for i,x in enumerate(x_values):
+    third_feature[i] = x*x*x
+
+design_matrix = np.c_[design_matrix, third_feature]
+
+# ----- (ii) -----
+
+# -----[4b] (ii) {third order gradient descent} -----
+# calculating difference between true values of theat and theta predicted by gradient descent
+
+gradient_third_order, ts, te = Gradient_Decent(4,0.000000001, 10**(-6), 0);
+
+theta_third_order = np.append(theta, 0)
+print("True values of Theat: ", theta_third_order)
+print("Gradient descent Theta: ", gradient_third_order)
+print("difference between True and predicted values: ", theta_third_order - gradient_third_order)
+print("\n")
+
+# ----- [4b] (iii) {third order gradient descent} -----
+# computing errrors in validation and training for gradient descent
+
+print("Training error for gradient descent: ", Error(gradient_third_order, training))
+print("Validation error for gradient descent: ", Error(gradient_third_order, validation))
+print("\n")
+
+# ----- [4b] (iv) {third order gradient descent} -----
+# plotting the model
+
+PlotScatter()
+plt.plot(x_train[:,1], f(x_train[:,1], gradient_third_order) ,"r")
+plt.title("Third order gradient descent solution (Overfitted)")
+plt.show()
+
+# ----- [4b] (v) {Third order gradient descent} -----
+# plot error time 
+
+plt.plot(ts[:100:], te[:100:], "y")
+plt.title("plot for training error for regression model over time ")
+plt.show()
+
+# ----- (iii) -----
+# Training using third orer gradient descent with regularization
+
+# lambda set to 0.3 
+
+gradient_reg, ts, te = Gradient_Decent(4, 0.000000001, 10**(-6), 0.3)
+print("Third Order gradient descent (no regularization): ")
+print("------------------------------------------------\n")
+
+# -----[4b] (ii) {third order gradient descent with regularization} -----
+# calculating difference between true values of theat and theta predicted by gradient descent
+
+print("True values of Theat: ", theta_third_order)
+print("Gradient descent Theta: ", gradient_reg)
+print("difference between True and predicted values: ", theta_third_order - gradient_reg)
+print("\n")
+
+# ----- [4b] (iii) {third order gradient descent with regularization} -----
+# computing errrors in validation and training for gradient descent
+
+print("Training error for third order gradient descent (regularization): ", Error(gradient_reg, training))
+print("Validation error for gradient descent (regularization): ", Error(gradient_reg, validation))
+print("\n")
+
+# ----- [4b] (iv) {third order gradient descent with regularization} -----
+# plotting the model
+
+PlotScatter()
+plt.plot(x_train[:,1], f(x_train[:,1], gradient_reg) ,"r")
+plt.title("Third order gradient descent solution with regularization")
+plt.show()
+
+# ----- [4b] (v) {Third order gradient descent with regularization} -----
+# plot error time 
+
+plt.plot(ts[:100:], te[:100:], "y")
+plt.title("plot for training error for regression model over time ")
+plt.show()
     
-    def Partial_Derivative_regularised(alpha, i, theta, l):
-        err = 0;
-        for j,d in enumerate(training):
-            y_hat = f(d[0], theta)
-            err += alpha*(y_hat - d[1])*(d[0]**i)
+#     def Partial_Derivative_regularised(alpha, i, theta, l):
+#         err = 0;
+#         for j,d in enumerate(training):
+#             y_hat = f(d[0], theta)
+#             err += alpha*(y_hat - d[1])*(d[0]**i)
 
-        return err + alpha*l*theta[i]
+#         return err + alpha*l*theta[i]
 
-    def Gradient_Decent_Regularised():
-        alpha = 0.00000000001
-        e = 10**(-5)
-        l = 0.3
+#     def Gradient_Decent_Regularised():
+#         alpha = 0.00000000001
+#         e = 10**(-5)
+#         l = 0.3
 
-        theta_old = np.zeros(4)
-        theta_new = np.random.uniform(0,1,4)
-        while (np.linalg.norm(abs(theta_old-theta_new),2) >= e):
-            for i in range(4):
-                theta_old[i] = theta_new[i]
-                pDer = Partial_Derivative_regularised(alpha, i, theta_old, l)
-                print(pDer)
-                theta_new[i] = theta_new[i] - pDer
+#         theta_old = np.zeros(4)
+#         theta_new = np.random.uniform(0,1,4)
+#         while (np.linalg.norm(abs(theta_old-theta_new),2) >= e):
+#             for i in range(4):
+#                 theta_old[i] = theta_new[i]
+#                 pDer = Partial_Derivative_regularised(alpha, i, theta_old, l)
+#                 theta_new[i] = theta_new[i] - pDer
     
-            # time_step = np.append(time_step,index)
-            # index = index +1
-            # if (index%20 == 0):
-            #     training_error = np.append(training_error,Error(theta_new))
+#             # time_step = np.append(time_step,index)
+#             # index = index +1
+#             # if (index%20 == 0):
+#             #     training_error = np.append(training_error,Error(theta_new))
     
-        return theta_new;
+#         return theta_new;
 
     
-    cubic_theta = Gradient_Decent_Regularised()
-    PlotScatter(trained, gradient, cubic_theta, x_train)
+#     cubic_theta = Gradient_Decent_Regularised()
+#     PlotScatter(trained, gradient, cubic_theta, x_train)
 
 
-Regularisation()
+# Regularisation()
